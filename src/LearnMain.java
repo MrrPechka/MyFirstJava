0c2c47bf1eee96d07d45f90a83445aa9d937d4ef
Huk Anna <gukanna606@gmail.com>
2020-09-27 18:15:39 +0300

    first commit

---
+++ src/LearnMain.java
@@ -0,0 +1,9 @@
+public class LearnMain {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 11);
+        System.out.println("Здравствуйте!");
+        
+    }
+}
